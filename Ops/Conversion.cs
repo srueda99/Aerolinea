8ea8f5979086daa2019f7fe96d12e536bd0b86c4
srueda99 <ruedamarinsebastian@gmail.com>
2022-05-09 20:16:18 -0500

    Operaciones

---
+++ Ops/Conversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ops
+{
+    public class Conversion
+    {
+        public static double ConvertirPesos(double usd)
+        {
+            try
+            {
+                double pesos = usd * 4080;
+                return pesos;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error encontrado al convertir a Pesos: {0}", e.Message);
+                return -1;
+            }
+        }
+        public static double ConvertirUSD(double pesos)
+        {
+            try
+            {
+                double usd = pesos / 4080;
+                return usd;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error encontrado al convertir a USD: {0}", e.Message);
+                return -1;
+            }
+        }
+    }
+}
---
+++ Ops/Haversine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ops
+{
+    public class Haversine
+    {
+        public static double CalcularDistancia(double lat1, double lon1, double lat2, double lon2)
+        {
+            try
+            {
+                double latOrg = (lat1 / (180 / Math.PI));
+                double lonOrg = (lon1 / (180 / Math.PI));
+                double latDest = (lat2 / (180 / Math.PI));
+                double lonDest = (lon2 / (180 / Math.PI));
+
+                double dist = Math.Sin(latDest) * Math.Sin(latOrg);
+                dist += Math.Cos(latDest) * Math.Cos(latOrg) * Math.Cos(lonDest - lonOrg);
+                dist = Math.Acos(dist) * 6371;
+                return dist;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error encontrado al calcular la distancia: {0}", e.Message);
+                return -1;
+            }
+        }
+    }
+}
